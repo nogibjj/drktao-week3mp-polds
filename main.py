cf2552917c83c44d60a843ed58a65cecdebdbbf7
drktao <83025663+drktao@users.noreply.github.com>
2023-09-18 01:19:18 +0000

    polars

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
-import pandas as pd
+import polars as pl
 import matplotlib.pyplot as plt
 import statistics
 
+
 def denirostats(file):
-    df=pd.read_csv(file)
-    sumstats=pd.DataFrame({'Mean Score':round(df.iloc[:,1].mean(),2),
-                           'Median Score':round(df.iloc[:,1].median(),2),
-                           'Standard Deviation of Scores':round(statistics.stdev(df.iloc[:,1]),2)},
-                           index=[0])
+    df = pl.read_csv(file)
+    sumstats = pl.DataFrame(
+        {
+            "Mean Score": round(df[:, 1].mean(), 2),
+            "Median Score": round(df[:, 1].median(), 2),
+            "Standard Deviation of Scores": round(statistics.stdev(df[:, 1]), 2),
+        }
+    )
     return sumstats
 
+
 def denirohist(file):
-    df=pd.read_csv(file)
-    plt.hist(df.iloc[:,1],bins=[0,10,20,30,40,50,60,70,80,90,100],edgecolor='black')
+    df = pl.read_csv(file)
+    plt.hist(
+        df[:, 1], bins=[0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100], edgecolor="black"
+    )
     plt.title("Rotten Tomatoes Score Distribution of Robert De Niro Movies")
     plt.xlabel("Score")
     plt.ylabel("Frequency")
     plt.show()
 
-print(denirostats('deniro.csv'))
-denirohist('deniro.csv')
+
+print(denirostats("deniro.csv"))
+denirohist("deniro.csv")
--- test_main.py
+++ test_main.py
@@ -1,10 +1,12 @@
 from main import denirostats
 from main import denirohist
 
+
 def test_deniro():
-    assert denirostats('deniro.csv').iloc[0,0]==58.20
-    assert denirostats('deniro.csv').iloc[0,1]==65.00
-    assert denirostats('deniro.csv').iloc[0,2]==28.07
+    assert denirostats("deniro.csv")[0, 0] == 58.20
+    assert denirostats("deniro.csv")[0, 1] == 65.00
+    assert denirostats("deniro.csv")[0, 2] == 28.07
+
 
 def test_viz():
-    denirohist('deniro.csv')
+    denirohist("deniro.csv")
